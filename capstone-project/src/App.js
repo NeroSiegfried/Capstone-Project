f9bf4a352fbc6d2af3e2eaca396ea8e57282a462
Victor <nerosiegfried@gmail.com>
2024-02-05 13:32:22 +0100

    added index.html, logo, main.js and other edits

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -2,9 +2,60 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Home Page</p>
-    </div>
+    <>
+    <nav>
+      <img src="/images/logo.jpg" alt="logo"></img>
+      <ul>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">ABOUT</a>
+        </li>
+        <li>
+          <a href="#">MENU</a>
+        </li>
+        <li>
+        <a href="#">RESERVATIONS</a>
+        </li>
+        <li>
+        <a href="#">ORDER ONLINE</a>
+        </li>
+        <li>
+        <a href="#">LOGIN</a>
+        </li>
+      </ul>
+    </nav>
+    <main/>
+    <footer>
+      <img src="#" alt="logo"></img>
+      <ul>
+        <li>
+          Doormat Navigation
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">Reservations</a>
+            </li>
+            <li>
+              <a href="#">Order Online</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </footer>
+    </>
   );
 }
 
